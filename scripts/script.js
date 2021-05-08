71a0924988e41a2212c9c2e6776135bbf1657a55
HasarAli <hasar.ali@protonmail.com>
2021-05-08 16:39:24 +0300

    refactored: moved randomHex to static Colors function

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,27 @@ class Colors {
   addProfiles(profile) {
     this.profile.push(profile);
   }
+
+  static get RandomHex() {
+    const hexCode = "0123456789ABCDEF";
+    let hex = '#';
+    for (let i = 0; i < 6; i++)
+      hex += hexCode[Math.floor(Math.random() * 16)];
+
+    return hex;
+  }
 }
 
 const userColors = new Color();
 
 function getRandomHex() {
-  let color = '#';
 
-  for (let i = 0; i < 6; i++)
-    Color += hexCode[Math.floor(Math.random() * 16)];
-
-  return color;
 }
+
+document.querySelector(".colors")
+  .children.forEach(color => {
+    const randomHex = getRandomHex;
+    color.style.backgroundColor = randomHex;
+    color.querySelector('#hex-value')
+      .innerText = randomHex;
+  });
